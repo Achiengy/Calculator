b4973dbb10a0a2fb92bd3574772e095dd0cb7ee1
Achiengy <petehope7@gmail.com>
2023-05-02 01:09:03 +0300

    rm text fit

--- src/components/Calc-screen.js
+++ src/components/Calc-screen.js
@@ -1,11 +1,11 @@
-import { Textfit } from "react-textfit";
+//import { Textfit } from "react-textfit";
 import './Calc-screen.css';
 
 function Screen ({value}) {
     return (
-         <Textfit className="screen" mode="single" max={70}>
+         <div className="screen" mode="single" max={70}>
             {value}
-         </Textfit>
+         </div>
          )
 };
 
